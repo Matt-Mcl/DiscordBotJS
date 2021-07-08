261da89aa50aeaac5bd51ceb070e78a9dee27485
Matt0905 <46825394+Matt-Mcl@users.noreply.github.com>
2021-07-08 08:54:46 +0000

    Fix for .climbing sometimes showing incorrectly
    Single digit climbing counts resulted in showing at 3,/85 for example.

--- commands/climbing/climbing.js
+++ commands/climbing/climbing.js
@@ -9,6 +9,9 @@ module.exports = {
             const response = await fetch('https://portal.rockgympro.com/portal/public/2660c1de4a602e808732f0bcd3fea712/occupancy?&iframeid=occupancyCounter&fId=');
             const text = await response.text();
             let count = text.match(/('count' : ).{1,2}/)[0].substring(10,12);
+            if (count.substring(count.length - 1) === ',') {
+                count = count.charAt(0);
+            }
 
             msg.channel.send(`There are ${count}/85 people climbing`);
         })()
