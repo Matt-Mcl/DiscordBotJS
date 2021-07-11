aac40eec002790171039b7c4af6cecee1faabae4
mm02417 <mm02417@surrey.ac.uk>
2021-07-11 18:48:46 +0100

    Disables cleardata command

--- commands/climbing/cleardata.js
+++ commands/climbing/cleardata.js
@@ -3,26 +3,26 @@ const redisScan = require('node-redis-scan');
 module.exports = {
     name: 'cleardata',
     group: 'climbing',
-    description: '```.cleardata \nClears all stored climbing data```',
+    description: '```.cleardata \n{CURRENTLY DISABLED} Clears all stored climbing data```',
     async execute(msg, args, redisClient) {
-        const scanner = new redisScan(redisClient);
+        // const scanner = new redisScan(redisClient);
 
-        let keys = await new Promise((resolve, reject) => {
-            scanner.scan('Climbing count: *', (err, matches) => {
-                resolve(matches);
-            });
-        });
+        // let keys = await new Promise((resolve, reject) => {
+        //     scanner.scan('Climbing count: *', (err, matches) => {
+        //         resolve(matches);
+        //     });
+        // });
 
-        for (let key of keys) {
-            let value = await new Promise((resolve, reject) => {
-                redisClient.del(key, function(err, reply) {
-                    resolve(reply);
-                });
-            });
+        // for (let key of keys) {
+        //     let value = await new Promise((resolve, reject) => {
+        //         redisClient.del(key, function(err, reply) {
+        //             resolve(reply);
+        //         });
+        //     });
 
-        }  
+        // }  
 
-        msg.channel.send('Cleared all data');
+        // msg.channel.send('Cleared all data');
 
     },
 };
