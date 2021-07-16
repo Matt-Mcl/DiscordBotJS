8849ac31faf899174ef44457ed42016631465b72
mm02417 <mm02417@surrey.ac.uk>
2021-07-16 21:46:38 +0100

    Adds exec command

---
+++ commands/general/exec.js
@@ -0,0 +1,15 @@
+const execSync = require('child_process').execSync;
+
+module.exports = {
+    name: 'exec',
+    group: 'general',
+    description: '```.exec [command] [args]\nAllows running commands in terminal```',
+    execute(msg, args) {
+        try {
+            const output = execSync(args.join(' '), { encoding: 'utf-8' });
+            msg.channel.send(output);
+        } catch (e) {
+            msg.channel.send(e.stderr);
+        }
+    },
+};
