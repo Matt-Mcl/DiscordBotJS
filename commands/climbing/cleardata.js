f65c0a2af568566e40a6f71b5ce0d32047110d33
mm02417 <mm02417@surrey.ac.uk>
2021-07-16 21:23:42 +0100

    Allows cleardata command to be run in development

--- commands/climbing/cleardata.js
+++ commands/climbing/cleardata.js
@@ -1,29 +1,29 @@
 const redisScan = require('node-redis-scan');
+require('dotenv').config();
 
 module.exports = {
     name: 'cleardata',
     group: 'climbing',
-    description: '```.cleardata \n{CURRENTLY DISABLED} Clears all stored climbing data```',
+    description: '```.cleardata \nClears all stored climbing data\n{Only enabled in development environment}```',
     async execute(msg, args, redisClient) {
-        // const scanner = new redisScan(redisClient);
+        if (process.env.ENVIRONMENT === 'PROD') return msg.channel.send('This command cannot be used in production');
+        const scanner = new redisScan(redisClient);
 
-        // let keys = await new Promise((resolve, reject) => {
-        //     scanner.scan('Climbing count: *', (err, matches) => {
-        //         resolve(matches);
-        //     });
-        // });
+        let keys = await new Promise((resolve, reject) => {
+            scanner.scan('Climbing count: *', (err, matches) => {
+                resolve(matches);
+            });
+        });
 
-        // for (let key of keys) {
-        //     let value = await new Promise((resolve, reject) => {
-        //         redisClient.del(key, function(err, reply) {
-        //             resolve(reply);
-        //         });
-        //     });
+        for (let key of keys) {
+            let value = await new Promise((resolve, reject) => {
+                redisClient.del(key, function(err, reply) {
+                    resolve(reply);
+                });
+            });
+        }  
 
-        // }  
-
-        // msg.channel.send('Cleared all data');
-
+        msg.channel.send('Cleared all data');
     },
 };
 
