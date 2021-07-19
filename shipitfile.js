f85be28ff33d5b52ccfa09c02a66cad9c54b93cc
Oracle Public Cloud User <opc@ampere.subnet07070859.vcn07070859.oraclevcn.com>
2021-07-19 11:25:43 +0000

    Configures ecosystem for pm2 instance

--- shipitfile.js
+++ shipitfile.js
@@ -4,12 +4,32 @@ module.exports = shipit => {
 
     shipit.initConfig({
         default: {
-            deployTo: '/var/node/shipit',
+            deployTo: '/home/opc/shipit',
             repositoryUrl: 'https://github.com/Matt-Mcl/DiscordBotJS.git',
+            key: '/home/opc/sshkey.key',
         },
-        key: '/var/node/ssh-key.key',
         staging: {
-            servers: 'deploy@152.67.150.176',
+            servers: 'opc@152.67.150.176',
         },
     })
+
+    shipit.blTask('pm2-setup', async () => {
+
+        // Eliminate potential previous demons 
+        await shipit.remote(`pm2 delete -s shipitbot || :`);
+
+        // Launch the npm package installation of the present deployment directory
+        await shipit.remote(`cd ${shipit.currentPath} && npm i`);
+
+        // Start the demon through pm2
+        await shipit.remote(
+            `pm2 start ${shipit.currentPath}/ecosystem.config.js`
+        );
+
+    });
+
+    shipit.on('deployed', async () => {
+        shipit.start('pm2-setup');
+    });
 }
+
