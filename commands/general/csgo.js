369f33dafd511e1a4130ccb21f9973b5f7a7c6d6
Matt0905 <mattmcl2000@gmail.com>
2021-07-28 12:04:37 +0100

    Adds csgo command functionality.

--- commands/general/csgo.js
+++ commands/general/csgo.js
@@ -1,24 +1,35 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const fetch = require('node-fetch');
 
 module.exports = {
     name: 'csgo',
     group: 'general',
-    description: '```TODO```',
+    description: '```.csgo [SteamID/VanityID] \nGets basic csgo stats for provided SteamID\nVanity IDs can be used```',
     async execute(msg, args) {
-        let request = new XMLHttpRequest();
-        request.open('GET', 'https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=//TODO&steamid=//TODO');
-        request.send();
+        if (args.length === 0) return msg.channel.send('Please provide a SteamID');
 
-        let response = null;
+        // Checks for VanityID
+        const getSteamID = await fetch(`http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${process.env.STEAMAPIKEY}&vanityurl=${args[0]}`);
+        const steamIDText = await getSteamID.text();
+        let steamID = JSON.parse(steamIDText)['response']['steamid'];
 
-        request.onload = () => {
-            response = JSON.parse(request.responseText);
+        // If no vanity ID is returned use base user input
+        if (!steamID) steamID = args[0];
 
-            let kills = response['playerstats']['stats'][0]['value'];
-            let deaths = response['playerstats']['stats'][1]['value'];
-            let headshots = response['playerstats']['stats'][25]['value'] * 100;
+        // Get csgo stats
+        const getStats = await fetch(`http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=${process.env.STEAMAPIKEY}&steamid=${steamID}`);
+        const statsText = await getStats.text();
 
-            msg.channel.send(`Kills: ${kills} | Deaths: ${deaths} | K/D Ratio: ${+((kills / deaths).toFixed(2))} | Headshot%: ${+((headshots / kills).toFixed(2))}`);
+        let stats;
+        try {
+            stats = JSON.parse(statsText);
+        } catch {
+            return msg.channel.send('Invalid SteamID');
         }
+
+        const kills = stats['playerstats']['stats'][0]['value'];
+        const deaths = stats['playerstats']['stats'][1]['value'];
+        const headshots = stats['playerstats']['stats'][25]['value'] * 100;
+
+        msg.channel.send(`Kills: ${kills} | Deaths: ${deaths} | K/D Ratio: ${+((kills / deaths).toFixed(2))} | Headshot%: ${+((headshots / kills).toFixed(2))}`);
     },
 };
