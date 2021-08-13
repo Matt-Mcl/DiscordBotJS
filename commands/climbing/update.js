56f4e38e081b52cb90f201b57397703ec174a524
mm02417 <mm02417@surrey.ac.uk>
2021-08-13 12:35:28 +0100

    Changes data url to use an environment variable

--- commands/climbing/update.js
+++ commands/climbing/update.js
@@ -7,7 +7,7 @@ module.exports = {
     description: '```.update \nOverwrites data in local database with data from production database\n{Only enabled in development environment}```',
     async execute(msg, args, redisClient) {
         if (process.env.ENVIRONMENT === 'PROD') return msg.channel.send('This command cannot be used in production');
-        const response = await fetch('https://climbing-app.co.uk/data');
+        const response = await fetch(`${process.env.API_SERVER}/data`);
         const json = await response.json();
 
         for (let item of json) {
