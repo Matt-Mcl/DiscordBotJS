79afb89fadadeb691f3a41ef5c311b2b6889c032
Matt Mcloughlin <46825394+Matt-Mcl@users.noreply.github.com>
2022-01-25 22:13:28 +0000

    Disables exec command

--- commands/general/exec.js
+++ commands/general/exec.js
@@ -5,6 +5,7 @@ module.exports = {
     group: 'general',
     description: `${process.env.PREFIX}exec [command] [args]\n\nAllows running commands in terminal`,
     execute(msg, args) {
+        return
         try {
             const output = execSync(args.join(' '), { encoding: 'utf-8' });
             if (output) msg.channel.send(output);
